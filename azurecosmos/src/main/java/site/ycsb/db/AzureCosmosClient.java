a5c6fd69b2278a02c4243490aa1b8ebcc2855230
Darshan Patnekar <darshanpatnekar@gmail.com>
2025-04-02 09:55:12 -0700

    Updated the code to use managed identity

--- azurecosmos/src/main/java/site/ycsb/db/AzureCosmosClient.java
+++ azurecosmos/src/main/java/site/ycsb/db/AzureCosmosClient.java
@@ -16,18 +16,19 @@
 
 package site.ycsb.db;
 
+import com.azure.core.credential.AccessToken;
+import com.azure.core.credential.TokenCredential;
+import com.azure.core.credential.TokenRequestContext;
 import com.azure.cosmos.ConsistencyLevel;
 import com.azure.cosmos.CosmosClient;
 import com.azure.cosmos.CosmosClientBuilder;
 import com.azure.cosmos.CosmosContainer;
 import com.azure.cosmos.CosmosDatabase;
-import com.azure.cosmos.CosmosDiagnosticsThresholds;
 import com.azure.cosmos.CosmosException;
 import com.azure.cosmos.DirectConnectionConfig;
 import com.azure.cosmos.GatewayConnectionConfig;
 import com.azure.cosmos.ThrottlingRetryOptions;
 import com.azure.cosmos.implementation.apachecommons.lang.StringUtils;
-import com.azure.cosmos.models.CosmosClientTelemetryConfig;
 import com.azure.cosmos.models.CosmosItemRequestOptions;
 import com.azure.cosmos.models.CosmosItemResponse;
 import com.azure.cosmos.models.CosmosPatchOperations;
@@ -44,6 +45,7 @@ import com.fasterxml.jackson.databind.node.ObjectNode;
 import io.micrometer.core.instrument.Counter;
 import io.micrometer.core.instrument.Metrics;
 import io.micrometer.core.instrument.Timer;
+import reactor.core.publisher.Mono;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.Marker;
@@ -79,7 +81,7 @@ public class AzureCosmosClient extends DB {
   // Default configuration values
   private static final ConsistencyLevel DEFAULT_CONSISTENCY_LEVEL = ConsistencyLevel.SESSION;
   private static final String DEFAULT_DATABASE_NAME = "ycsb";
-  private static final boolean DEFAULT_USE_GATEWAY = false;
+  private static final boolean DEFAULT_USE_GATEWAY = true;
   private static final boolean DEFAULT_USE_UPSERT = false;
   private static final int DEFAULT_MAX_DEGREE_OF_PARALLELISM = -1;
   private static final int DEFAULT_MAX_BUFFERED_ITEM_COUNT = 0;
@@ -93,7 +95,6 @@ public class AzureCosmosClient extends DB {
   private static final Marker READ_DIAGNOSTIC = MarkerFactory.getMarker("READ_DIAGNOSTIC");
   private static final Marker PATCH_DIAGNOSTIC = MarkerFactory.getMarker("PATCH_DIAGNOSTIC");
   private static final Marker DELETE_DIAGNOSTIC = MarkerFactory.getMarker("DELETE_DIAGNOSTIC");
-  private static final Marker QUERY_DIAGNOSTIC = MarkerFactory.getMarker("QUERY_DIAGNOSTIC");
   private static final Marker CREATE_EXCEPTION = MarkerFactory.getMarker("CREATE_EXCEPTION");
   private static final Marker READ_EXCEPTION = MarkerFactory.getMarker("READ_EXCEPTION");
   private static final Marker PATCH_EXCEPTION = MarkerFactory.getMarker("PATCH_EXCEPTION");
@@ -134,6 +135,38 @@ public class AzureCosmosClient extends DB {
   private static Counter updateSuccessCounter;
   private static Counter updateFailureCounter;
   private static Timer updateSuccessLatencyTimer;
+
+  /**
+   * TokenCredential implementation for Azure Cosmos DB.
+   * This implementation uses InteractiveBrowserCredentialBuilder to authenticate
+   * with Azure AD.
+   */
+  public static class MyTokenCredential implements TokenCredential {
+    private final TokenCredential credential;
+
+    public MyTokenCredential(String managedIdentityClientId) {
+      this.credential = new DefaultAzureCredentialBuilder()
+          .managedIdentityClientId(managedIdentityClientId)
+          .authorityHost("https://login.microsoftonline.com/72f988bf-86f1-41af-91ab-2d7cd011db47")
+          .build();
+    }
+
+    @Override
+    public AccessToken getTokenSync(TokenRequestContext tokenRequestContext) {
+      TokenRequestContext cosmosTokenRequest = new TokenRequestContext();
+      cosmosTokenRequest.addScopes("https://cosmos.azure.com/.default");
+
+      return credential.getToken(cosmosTokenRequest).block();
+    }
+
+    @Override
+    public Mono<AccessToken> getToken(TokenRequestContext tokenRequestContext) {
+      TokenRequestContext cosmosTokenRequest = new TokenRequestContext();
+      cosmosTokenRequest.addScopes("https://cosmos.azure.com/.default");
+
+      return credential.getToken(cosmosTokenRequest);
+    }
+  }
 
   @Override
   public void init() throws DBException {
@@ -252,16 +285,11 @@ public class AzureCosmosClient extends DB {
 
       CosmosClientBuilder builder = new CosmosClientBuilder()
           .endpoint(uri)
-          .credential(new DefaultAzureCredentialBuilder().managedIdentityClientId(managedIdentityClientId).build())
+          .credential(new MyTokenCredential(managedIdentityClientId))
           .throttlingRetryOptions(retryOptions)
           .consistencyLevel(consistencyLevel)
-          .userAgentSuffix(userAgent)
-          .clientTelemetryConfig(new CosmosClientTelemetryConfig()
-              .diagnosticsThresholds(
-                  new CosmosDiagnosticsThresholds()
-                      .setPointOperationLatencyThreshold(Duration.ofMillis(pointOperationLatencyThresholdInMS))
-                      .setNonPointOperationLatencyThreshold(Duration.ofMillis(nonPointOperationLatencyThresholdInMS))
-                      .setRequestChargeThreshold(requestChargeThreshold)));
+          .gatewayMode()
+          .userAgentSuffix(userAgent);
 
       if (useGateway) {
         builder = builder.gatewayMode(gatewayConnectionConfig);
